f6e5010bf5e51a293e34d6bb5d8d0d0f8404e5e0
ITRS <basvrajhonrao@gmail.com>
2024-01-16 15:12:46 +0530

    RegisterPageTC added

---
+++ src/main/java/BaseLayer/BaseClass.java
@@ -0,0 +1,38 @@
+package BaseLayer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	public static Properties prop;
+	public static WebDriver driver;
+	public BaseClass()
+	{
+		File f =new File(System.getProperty("User.dir")+"src\\main\\java\\ConfigurationLayer\\face.properties");
+		try {
+			FileInputStream fis= new FileInputStream(f);
+			 prop=new Properties();
+			 prop.load(fis);
+			
+		} catch (Exception e) {
+		
+			e.printStackTrace();
+		}
+		
+	}
+	public void initialization()
+	{
+	 driver=new ChromeDriver();
+	 driver.manage().window().maximize();
+	 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+	 driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+	 driver.manage().deleteAllCookies();
+	 driver.get(prop.getProperty("URL"));
+		
+	}
+}
