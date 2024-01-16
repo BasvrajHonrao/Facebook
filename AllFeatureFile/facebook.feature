eef0ec668db828ed205b0b119ad82fd32a666723
ITRS <basvrajhonrao@gmail.com>
2024-01-16 13:58:53 +0530

    BDD Framework Created

---
+++ AllFeatureFile/facebook.feature
@@ -0,0 +1,6 @@
+Feature: Facebook Test
+
+  Scenario: Register Page Test\
+    Given user is on rgisterpage
+    When user enter valid fname,lname,emil,pass
+    And user enter date,month,year and gender
---
+++ src/test/java/Step/RegistrationPageSteps.java
@@ -0,0 +1,24 @@
+package Step;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+
+
+public class RegistrationPageSteps {
+	@Given("user is on rgisterpage")
+	public void user_is_on_rgisterpage() {
+	   ;
+	}
+
+	@When("user enter valid fname,lname,emil,pass")
+	public void user_enter_valid_fname_lname_emil_pass() {
+	 
+	}
+
+	@When("user enter date,month,year and gender")
+	public void user_enter_date_month_year_and_gender() {
+	  
+
+}
+}
